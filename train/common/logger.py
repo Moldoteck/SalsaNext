46dc6c2d21627379908bc848a4aba082e280d8a5
Padureac, Cristian (PERO-SA) <cristian.padureac@porsche-engineering.ro>
2021-05-13 10:06:00 +0300

    fix log problem

--- train/common/logger.py
+++ train/common/logger.py
@@ -19,35 +19,43 @@ class Logger(object):
 
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
-        summary = tf.Summary(
-            value=[tf.Summary.Value(tag=tag, simple_value=value)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.scalar(tag, value, step)
         self.writer.flush()
+        # summary = tf.Summary(
+        #     value=[tf.Summary.Value(tag=tag, simple_value=value)])
+        # self.writer.add_summary(summary, step)
+        # self.writer.flush()
 
     def image_summary(self, tag, images, step):
         """Log a list of images."""
 
         img_summaries = []
-        for i, img in enumerate(images):
+        with self.writer.as_default():
+            for i, img in enumerate(images):
+                tf.summary.image('%s/%d' % (tag, i), [img], step)
+        self.writer.flush()
+        # for i, img in enumerate(images):
             # Write the image to a string
-            try:
-                s = StringIO()
-            except:
-                s = BytesIO()
-            scipy.misc.toimage(img).save(s, format="png")
+            # try:
+            #     s = StringIO()
+            # except:
+            #     s = BytesIO()
+            # scipy.misc.toimage(img).save(s, format="png")
 
+            
             # Create an Image object
-            img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
-                                       height=img.shape[0],
-                                       width=img.shape[1])
-            # Create a Summary value
-            img_summaries.append(tf.Summary.Value(
-                tag='%s/%d' % (tag, i), image=img_sum))
+            # img_sum = tf.Summary.Image(encoded_image_string=s.getvalue(),
+            #                            height=img.shape[0],
+            #                            width=img.shape[1])
+            # # Create a Summary value
+            # img_summaries.append(tf.Summary.Value(
+            #     tag='%s/%d' % (tag, i), image=img_sum))
 
         # Create and write Summary
-        summary = tf.Summary(value=img_summaries)
-        self.writer.add_summary(summary, step)
-        self.writer.flush()
+        # summary = tf.Summary(value=img_summaries)
+        # self.writer.add_summary(summary, step)
+        # self.writer.flush()
 
     def histo_summary(self, tag, values, step, bins=1000):
         """Log a histogram of the tensor of values."""
@@ -73,6 +81,9 @@ class Logger(object):
             hist.bucket.append(c)
 
         # Create and write Summary
-        summary = tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
-        self.writer.add_summary(summary, step)
+        with self.writer.as_default():
+            tf.summary.histogram(tag, hist, step)
         self.writer.flush()
+        # summary = tf.Summary(value=[tf.Summary.Value(tag=tag, histo=hist)])
+        # self.writer.add_summary(summary, step)
+        # self.writer.flush()
