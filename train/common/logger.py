96bad81409641126221969c96dfefa4d8ccdfe30
Padureac, Cristian (PERO-SA) <cristian.padureac@porsche-engineering.ro>
2021-05-13 15:24:14 +0300

    summary error

--- train/common/logger.py
+++ train/common/logger.py
@@ -20,7 +20,7 @@ class Logger(object):
     def scalar_summary(self, tag, value, step):
         """Log a scalar variable."""
         with self.writer.as_default():
-            tf.summary.scalar(tag, value, step)
+            tf.summary.scalar(tag, value.argmax(), step)
         self.writer.flush()
         # summary = tf.Summary(
         #     value=[tf.Summary.Value(tag=tag, simple_value=value)])
