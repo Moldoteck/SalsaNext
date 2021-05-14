6d57c2a890ffa0afb1ec3b62569f98c3d0f41b6e
Padureac, Cristian (PERO-SA) <cristian.padureac@porsche-engineering.ro>
2021-05-14 21:46:05 +0300

    check np

--- train/common/logger.py
+++ train/common/logger.py
@@ -21,7 +21,7 @@ class Logger(object):
         """Log a scalar variable."""
         with self.writer.as_default():
             print(value)
-            if isinstance(value, (np.floating,float)):
+            if isinstance(value, (np.floating,float, int)):
                 tf.summary.scalar(tag, value, step)
             else:
                 tf.summary.scalar(tag, value.cpu(), step)
