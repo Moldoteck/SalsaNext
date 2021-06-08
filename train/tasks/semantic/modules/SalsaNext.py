cf37ea6c9a54d299a4660b017a59a572a98deb35
Cristian Padureac <cpadureac@gmail.com>
2021-06-08 18:50:50 +0300

    involution context

--- train/tasks/semantic/modules/SalsaNext.py
+++ train/tasks/semantic/modules/SalsaNext.py
@@ -11,23 +11,32 @@ import deepspeed
 
 
 class ResContextBlock(nn.Module):
-    def __init__(self, in_filters, out_filters):
+    def __init__(self, in_filters, out_filters, invol = False):
         super(ResContextBlock, self).__init__()
         global checkpoint
         checkpoint = deepspeed.checkpointing.checkpoint
+        self.invol = invol
         self.deepspeed_checkpointing = False
         self.conv1 = nn.Conv2d(in_filters, out_filters, kernel_size=(1, 1), stride=1)
         self.act1 = nn.LeakyReLU()
 
-        self.conv2 = nn.Conv2d(out_filters, out_filters, (3,3), padding=1)
-        self.act2 = nn.LeakyReLU()
-        self.bn1 = nn.BatchNorm2d(out_filters)
+        if invol:
+            self.invo2 = Involution2d(out_filters, out_filters, 
+            sigma_mapping = nn.Sequential(nn.LeakyReLU(), nn.BatchNorm2d(out_filters)),
+            kernel_size=(3,3),stride=1, padding=(1,1), dilation=(1,1))
+        else:
+            self.conv2 = nn.Conv2d(out_filters, out_filters, (3,3), padding=1)
+            self.act2 = nn.LeakyReLU()
+            self.bn1 = nn.BatchNorm2d(out_filters)
 
-        # self.invo3 = Involution2d(in_channels=out_filters,out_channels= out_filters, sigma_mapping = nn.Sequential(nn.LeakyReLU(), nn.BatchNorm2d(out_filters)),kernel_size=(3,3),stride=1, padding=(2,2), dilation=(2,2))
-        
-        self.conv3 = nn.Conv2d(out_filters, out_filters, (3,3),dilation=2, padding=2)
-        self.act3 = nn.LeakyReLU()
-        self.bn2 = nn.BatchNorm2d(out_filters)
+        if invol:
+            self.invo3 = Involution2d(out_filters, out_filters, 
+            sigma_mapping = nn.Sequential(nn.LeakyReLU(), nn.BatchNorm2d(out_filters)),
+            kernel_size=(3,3),stride=1, padding=(2,2), dilation=(2,2))
+        else:
+            self.conv3 = nn.Conv2d(out_filters, out_filters, (3,3),dilation=2, padding=2)
+            self.act3 = nn.LeakyReLU()
+            self.bn2 = nn.BatchNorm2d(out_filters)
 
     def forward(self, x):
         if self.deepspeed_checkpointing:
@@ -45,14 +54,17 @@ class ResContextBlock(nn.Module):
             shortcut = self.conv1(x)
             shortcut = self.act1(shortcut)
 
-            resA = self.conv2(shortcut)
-            resA = self.act2(resA)
-            resA1 = self.bn1(resA)
+            if self.invol:
+                resA1 = self.invo2(shortcut)
+                resA2 = self.invo3(resA1)
+            else:
+                resA = self.conv2(shortcut)
+                resA = self.act2(resA)
+                resA1 = self.bn1(resA)
 
-            resA = self.conv3(resA1)
-            resA = self.act3(resA)
-            resA2 = self.bn2(resA)
-            # resA2 = self.invo3(resA1)
+                resA = self.conv3(resA1)
+                resA = self.act3(resA)
+                resA2 = self.bn2(resA)
         output = shortcut + resA2
         return output
 
